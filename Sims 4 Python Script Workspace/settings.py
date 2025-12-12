5d59503d01b854d1d997ea568dbcbafdbb8abeb9
Gabi <92929485+Delimaci@users.noreply.github.com>
2025-12-12 14:00:31 +0000

    Add files via upload

---
+++ Sims 4 Python Script Workspace/settings.py
@@ -0,0 +1,8 @@
+import os
+
+creator_name = 'Delimaci'
+mods_folder = os.path.expanduser(
+os.path.join('~', 'Documents', 'Electronic Arts','The Sims 4','Mods')
+)
+
+game_folder = os.path.join('D:', 'Games', 'EA Games', 'The Sims 4')
